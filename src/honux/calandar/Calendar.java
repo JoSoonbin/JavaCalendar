7e96268902ecabcf601bbdc4059f662cad02d065
뎅뎅 <soonbin30@gmail.com>
2019-11-08 00:31:23 +0900

    Implement find max days of the month

--- src/honux/calandar/Calendar.java
+++ src/honux/calandar/Calendar.java
@@ -1,5 +1,7 @@
 package honux.calandar;
 
+import java.util.Scanner;
+
 public class Calendar {
 	public static void main(String[] args) {
 		//System.out.println("Hello, Calendar");
@@ -9,5 +11,49 @@ public class Calendar {
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		
+		
+		//숫자를 입력받아 해당하는 달의 최대일수를 출력하는 프로그램
+		
+		//조건문 사용
+//		int month, day;
+//		System.out.println("달을 입력하세요.");
+//		Scanner scanner = new Scanner(System.in);
+//		String s1;
+//		s1 = scanner.next();
+//		month = Integer.parseInt(s1);
+//		scanner.close();
+		
+//		if(month == 2)
+//			day = 28;
+//		else if(month % 2 == 1 || month == 8)
+//			day = 31;
+//		else
+//			day = 30;
+		
+//		System.out.printf("%d월은 %d일까지 있습니다.", month, day);
+		
+		//배열 사용
+		
+//		int month;
+//		System.out.println("달을 입력하세요.");
+//		Scanner scanner = new Scanner(System.in);
+//		String s1;
+//		s1 = scanner.next();
+//		month = Integer.parseInt(s1);
+//		scanner.close();
+		
+//		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+//		System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month-1]);
+		
+		
+		//강의 풀이
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("달을 입력하세요");
+		int month = scanner.nextInt();
+		
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month-1]);
+		scanner.close();
 	}
 }
